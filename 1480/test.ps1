ed26deaf8f56a38ee52c4bba44d85fe004f77fef
stundner,gabriel <gabriel.stundner@dell.com>
2024-04-03 22:15:37 -0300

    Added Test Enviroments

--- 1480/test.ps1
+++ 1480/test.ps1
@@ -1,3 +1,3 @@
 Write-Host  'Running the tests...' -ForegroundColor Magenta
 Write-Host ''
-dotnet test RunningSumTest/RunningSumTest.csproj
+dotnet test --nologo --logger "console;verbosity=detailed" .\RunningSumTest\RunningSumTest.csproj
